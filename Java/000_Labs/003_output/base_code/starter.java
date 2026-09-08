1126497a6167422c786267d8cb2b664847bf156e
Davo <pbd86vtznz@privaterelay.appleid.com>
2026-09-08 18:01:27 +0000

    update

--- Java/000_Labs/003_output/base_code/starter.java
+++ Java/000_Labs/003_output/base_code/starter.java
@@ -1,5 +1,5 @@
 /*
- *	Author:  
+ *	Author:  Davo Har
  *  Date: 
 */
 
@@ -9,6 +9,10 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("Happy Birthday, to you.");
+		System.out.println("Happy Birthday, to you.");
+		System.out.println("Happy Birthday dear Davit,");
+		System.out.print("Happy Birthday, to you.");
+
 	}
 }
