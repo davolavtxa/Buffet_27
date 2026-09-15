aa3f042d2473b0dfb1adeb0a3159b119a54f95c7
Davo <pbd86vtznz@privaterelay.appleid.com>
2026-09-15 18:26:01 +0000

    a

--- Java/101_Lecture_Notes/LectureInput.java
+++ Java/101_Lecture_Notes/LectureInput.java
@@ -1,9 +1,29 @@
 /*
     Lecture note example - Input!!
 */
+import java.util.Scanner;
 
 class LectureInput{
     public static void main(String args[]) {
-        // Your Code Goes here!
+     System.out.println("Welcome to Har's Khinkalnoc");
+     System.out.println("Here is our Menu!:");
+     System.out.print("1. Lamb Khinkali:" );
+     double itemPrice = 3.50;
+
+     System.out.println(itemPrice);
+    System.out.print("2.  Pork Khinkali:" );
+     double item2Price = 3.50;
+     System.out.println(item2Price);
+     
+     System.out.print("3. Fried Lamb Khinkali:" );
+     double item3Price = 4.50;
+     System.out.println(item3Price);
+
+     System.out.println("How many Khinkalis would you like?");
+     Scanner sc = new Scanner (System.in);
+
+     int item1Quant = sc.nextInt();
+     System.out.println("Item 1 Total is:" );
+     
 	}
 }
