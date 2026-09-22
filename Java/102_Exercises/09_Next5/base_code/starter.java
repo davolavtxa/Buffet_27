6d9e4a79904db2c2c7c7a4e7dc1fc35bbadca76f
Davo <pbd86vtznz@privaterelay.appleid.com>
2026-09-22 18:03:10 +0000

    a

--- Java/102_Exercises/09_Next5/base_code/starter.java
+++ Java/102_Exercises/09_Next5/base_code/starter.java
@@ -5,9 +5,21 @@
 */
 import java.util.Scanner;
 
-class starter {
-	public static void main(String args[]) {
+public class starter {
+    public static void  {
+        Scanner  = new Scanner(System.in);
 
+        int num = scanner.nextInt();
 
-	}
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(num + i);
+        }
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(num * i);
+        }
+
+        System.out.println(num / 100.0);
+        System.out.println(num / 10.0);
+    }
 }
