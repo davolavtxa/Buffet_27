98d8b08528a75b7ad0f34552de8aec22910296d1
Davo <pbd86vtznz@privaterelay.appleid.com>
2026-09-22 18:32:56 +0000

    a

--- Java/102_Exercises/12_Colors/base_code/starter.java
+++ Java/102_Exercises/12_Colors/base_code/starter.java
@@ -8,9 +8,13 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		
-
-		// Call getColor(#, #, #);
+	
+    int red = (int)	Math.random()* 256;
+	int green = (int)Math.random()* 256;
+    int blue = (int)Math.random()*256;
+	getColor(red, green, blue);
+    getColor(255-red, 255-green, 255-blue);
+    	// Call getColor(#, #, #);
 	}
 
 	public static void getColor(int red, int green, int blue){
