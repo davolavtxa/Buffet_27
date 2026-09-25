dcef8329783f992b21e8d24bdc17a04602699503
Davo <pbd86vtznz@privaterelay.appleid.com>
2026-09-25 17:55:26 +0000

    a

--- Java/000_Labs/005_int/base_code/starter.java
+++ Java/000_Labs/005_int/base_code/starter.java
@@ -7,8 +7,22 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		int math1 = (5);
+		int math2 = (7);
+		int math3 = (2);
+		int math4 = (3);
+		int math5 = (1);
+
+		int Math1 = (10);
+		int Math2 = (3);
+		int Math3 = (4);
+		int Math4 = (7);
+		int Math5 = (2);
+
+		System.out.println(math1 * math2 / math3 % math4-math5);
+		System.out.println(Math1 % Math2 - Math3 * Math4 + Math5);
+		
+		
+
 	}
 }
--- Java/000_Labs/011_if_statements/base_code/starter.java
+++ Java/000_Labs/011_if_statements/base_code/starter.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		let result = prompt();
-		if (prompt <=13) {
+		int = ();
+		if ( >=13) {
 			System.out.println("Welcome");
 
 		}
